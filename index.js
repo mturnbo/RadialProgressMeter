dc24c07a0f18000bf38c6082ef1eac6f266595be
Marcus Turnbo <mturnbo@gogoair.com>
2016-10-19 16:09:42 -0500

    Replaced all this

--- index.js
+++ index.js
@@ -72,56 +72,56 @@ function RadialProgressMeter(element, options) {
   }
 
   // create svg element
-  this.svg = document.createElementNS(svgns, 'svg');
-  this.svg.setAttribute('width', this.options.width);
-  this.svg.setAttribute('height', this.options.height);
+  self.svg = document.createElementNS(svgns, 'svg');
+  self.svg.setAttribute('width', self.options.width);
+  self.svg.setAttribute('height', self.options.height);
 
   // gradient
-  var grad = makeGradient('gradProgress', this.options.colors.meterStart,  this.options.colors.meterStop);
+  var grad = makeGradient('gradProgress', self.options.colors.meterStart,  self.options.colors.meterStop);
 
   var defs = document.createElementNS(svgns,'defs')
   defs.appendChild(grad);
 
-  this.svg.insertBefore(defs, this.svg.firstChild);
+  self.svg.insertBefore(defs, self.svg.firstChild);
 
   // background arc
-  this.meterBackground = document.createElementNS(svgns, 'path');
-  this.meterBackground.setAttribute('id', 'meterBackground');
-  this.meterBackground.setAttribute("d", arcAttributes(
+  self.meterBackground = document.createElementNS(svgns, 'path');
+  self.meterBackground.setAttribute('id', 'meterBackground');
+  self.meterBackground.setAttribute("d", arcAttributes(
       self.options.centerX,
       self.options.centerY,
       self.options.radius,
       self.options.startAngle,
       self.options.endAngle
   ));
-  this.meterBackground.style.fill = 'none';
-  this.meterBackground.style.stroke = this.options.colors.background;
-  this.meterBackground.style.strokeWidth = this.options.stroke;
-  this.meterBackground.style.strokeLinecap = 'round';
+  self.meterBackground.style.fill = 'none';
+  self.meterBackground.style.stroke = self.options.colors.background;
+  self.meterBackground.style.strokeWidth = self.options.stroke;
+  self.meterBackground.style.strokeLinecap = 'round';
 
-  this.svg.appendChild(this.meterBackground);
+  self.svg.appendChild(self.meterBackground);
 
   // progress arc
   var progressAngle = self.options.progress * (self.options.endAngle - self.options.startAngle) + self.options.startAngle;
-  this.meterProgress = document.createElementNS(svgns, 'path');
-  this.meterProgress.setAttribute('id', 'meterProgress');
-  this.meterProgress.setAttribute("d", arcAttributes(
+  self.meterProgress = document.createElementNS(svgns, 'path');
+  self.meterProgress.setAttribute('id', 'meterProgress');
+  self.meterProgress.setAttribute("d", arcAttributes(
       self.options.centerX,
       self.options.centerY,
       self.options.radius,
       self.options.startAngle,
       progressAngle
   ));
-  this.meterProgress.style.fill = 'none';
-  this.meterProgress.style.stroke = 'url(#gradProgress)';
-  this.meterProgress.style.strokeWidth = this.options.stroke;
-  this.meterProgress.style.strokeLinecap = 'round';
-  this.meterProgress.style.strokeDasharray = '';
-  this.meterProgress.style.strokeDashoffset = '0.00';
+  self.meterProgress.style.fill = 'none';
+  self.meterProgress.style.stroke = 'url(#gradProgress)';
+  self.meterProgress.style.strokeWidth = self.options.stroke;
+  self.meterProgress.style.strokeLinecap = 'round';
+  self.meterProgress.style.strokeDasharray = '';
+  self.meterProgress.style.strokeDashoffset = '0.00';
 
-  this.svg.appendChild(this.meterProgress);
+  self.svg.appendChild(self.meterProgress);
 
-  document.getElementById(element).appendChild(this.svg);
+  document.getElementById(element).appendChild(self.svg);
 
   // animate progress
   var progressPath = document.getElementById('meterProgress');
@@ -134,7 +134,7 @@ function RadialProgressMeter(element, options) {
   progressPath.getBoundingClientRect();
 
   progressPath.style.transition = progressPath.style.WedkitTransition = 'stroke-dashoffset '
-      + this.options.animation.duration + 'ms ease-out';
+      + self.options.animation.duration + 'ms ease-out';
 
   progressPath.style.strokeDashoffset = '0';
 

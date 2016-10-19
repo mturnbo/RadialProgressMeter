928c06bb7dd3aca8d0dc13c89e93d9f8f1248744
Marcus Turnbo <mturnbo@gogoair.com>
2016-10-19 11:09:00 -0500

    Added initial progress arc with gradient and background

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
-var d3;
-
-function RadialProgressMeter(query, options) {
-
-  // load D3
-  d3 = (typeof window !== 'undefined' && window.d3) ? window.d3 : typeof require !== 'undefined' ? require('d3') : undefined;
-  if (!d3) throw new Error('d3 object is missing. D3.js library has to be loaded before.');
+function RadialProgressMeter(backgroundElement, progressElement, options) {
 
   var self = this;
   self.options = options;
+
+  function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+    var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+    return {
+      x: centerX + (radius * Math.cos(angleInRadians)),
+      y: centerY + (radius * Math.sin(angleInRadians))
+    };
+  }
+
+  function arcAttributes(x, y, radius, startAngle, endAngle) {
+
+    var start = polarToCartesian(x, y, radius, endAngle);
+    var end = polarToCartesian(x, y, radius, startAngle);
+    var isLargeArc = endAngle - startAngle <= 180 ? "0" : "1";
+
+    return [
+      "M", start.x, start.y,
+      "A", radius, radius, 0, isLargeArc, 0, end.x, end.y
+    ].join(" ");
+  }
+
+  function validateOptions(options) {
+
+    if (!options || typeof options !== 'object') {
+      options = {};
+    }
+
+    return {
+      radius: options.radius || 100,
+      stroke: {
+        width: options.stroke && options.stroke.width || 40,
+        gap: options.stroke && options.stroke.gap || 2
+      },
+      animation: {
+        duration: options.animation && options.animation.duration || 1750,
+        delay: options.animation && options.animation.delay || 200
+      },
+      startAngle: options.startAngle || -120,
+      endAngle: options.endAngle || 120,
+      progress: options.progress || 0
+    };
+  }
+
+  // set background
+  document.getElementById(backgroundElement).setAttribute("d", arcAttributes(
+      self.options.centerX,
+      self.options.centerY,
+      self.options.radius,
+      self.options.startAngle,
+      self.options.endAngle
+  ));
+
+  // set progress
+  document.getElementById(progressElement).setAttribute("d", arcAttributes(
+      self.options.centerX,
+      self.options.centerY,
+      self.options.radius,
+      self.options.startAngle,
+      90
+  ));
 
 }
 

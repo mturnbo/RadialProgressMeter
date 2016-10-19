29f72495ece2c055ee33fcdc962c0e93b77ebc89
Marcus Turnbo <mturnbo@gogoair.com>
2016-10-19 12:08:44 -0500

    Switch to dynamic creation of svg elements

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function RadialProgressMeter(backgroundElement, progressElement, options) {
+function RadialProgressMeter(element, options) {
 
   var self = this;
+  var svgns = "http://www.w3.org/2000/svg"
+  var xns = "http://www.w3.org/2000/xlink/namespace/";
+
   self.options = validateOptions(options);
 
   function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
@@ -33,37 +36,80 @@ function RadialProgressMeter(backgroundElement, progressElement, options) {
       centerX: options.centerX || 150,
       centerY: options.centerY || 150,
       radius: options.radius || 100,
-      stroke: {
-        width: options.stroke && options.stroke.width || 25,
-        gap: options.stroke && options.stroke.gap || 2
-      },
+      stroke: options.stroke || 25,
       animation: {
         duration: options.animation && options.animation.duration || 1750,
         delay: options.animation && options.animation.delay || 200
       },
       startAngle: options.startAngle || -120,
       endAngle: options.endAngle || 120,
-      progress: options.progress || 0
+      progress: options.progress || 0,
+      colors: {
+        background: options.colors && options.colors.background || '#CECECE',
+        meterStart: options.colors && options.colors.meterStart || '#2DA4D8',
+        meterStop: options.colors && options.colors.meterStop || '#8CDAF9'
+      }
     };
   }
 
-  // set background
-  document.getElementById(backgroundElement).setAttribute("d", arcAttributes(
+  // create svg element
+  this.svg = document.createElementNS(svgns, 'svg');
+  this.svg.setAttribute('width', 400);
+  this.svg.setAttribute('height', 400);
+
+  // gradient
+  var grad = document.createElementNS(svgns, 'linearGradient');
+  grad.setAttribute('id', 'gradProgress');
+
+  var startStop = document.createElementNS(svgns, 'stop');
+  startStop.setAttribute('offset', '0%');
+  startStop.setAttribute('stop-color', this.options.colors.meterStart);
+  grad.appendChild(startStop);
+
+  var endStop = document.createElementNS(svgns, 'stop');
+  endStop.setAttribute('offset', '100%');
+  endStop.setAttribute('stop-color', this.options.colors.meterStop);
+  grad.appendChild(endStop);
+
+  var defs = document.createElementNS(svgns,'defs')
+  defs.appendChild(grad);
+
+  this.svg.insertBefore(defs, this.svg.firstChild);
+
+  // background arc
+  this.meterBackground = document.createElementNS(this.svg.namespaceURI, 'path');
+  this.meterBackground.setAttribute("d", arcAttributes(
       self.options.centerX,
       self.options.centerY,
       self.options.radius,
       self.options.startAngle,
       self.options.endAngle
   ));
+  this.meterBackground.style.fill = 'none';
+  this.meterBackground.style.stroke = this.options.colors.background;
+  this.meterBackground.style.strokeWidth = this.options.stroke;
+  this.meterBackground.style.strokeLinecap = 'round';
 
-  // set progress
-  document.getElementById(progressElement).setAttribute("d", arcAttributes(
+  this.svg.appendChild(this.meterBackground);
+
+  // progress arc
+  var progressAngle = self.options.progress * (self.options.endAngle - self.options.startAngle) + self.options.startAngle;
+  this.meterProgress = document.createElementNS(this.svg.namespaceURI, 'path');
+  this.meterProgress.setAttribute("d", arcAttributes(
       self.options.centerX,
       self.options.centerY,
       self.options.radius,
       self.options.startAngle,
-      90
+      progressAngle
   ));
+  this.meterProgress.style.fill = 'none';
+  this.meterProgress.style.stroke = 'url(#gradProgress)';
+  this.meterProgress.style.strokeWidth = this.options.stroke;
+  this.meterProgress.style.strokeLinecap = 'round';
+
+  this.svg.appendChild(this.meterProgress);
+
+  document.getElementById(element).appendChild(this.svg);
 
 }
 
